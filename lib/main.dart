da34cc9c3c74c79787c7a97a00a86980907cea26
sandipghosh8687-prog <sandipghosh8687@gmail.com>
2026-09-05 16:03:09 +0000

    Show friend requests only in Friends

--- lib/main.dart
+++ lib/main.dart
@@ -1028,6 +1028,7 @@ class _HomePageState extends State<HomePage> {
           ),
           const SizedBox(height: 15),
           ...notifications
+              .where((n) => n['request'] != true)
               .where(
                 (item) =>
                     !(item['request'] == true &&
