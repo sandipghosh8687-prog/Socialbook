b5b2c64f99eb7a39d33ee465f03c0298aa92376e
sandipghosh8687-prog <sandipghosh8687@gmail.com>
2026-09-05 11:19:42 +0000

    Deploy responsive layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1901607042" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "644619355" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
